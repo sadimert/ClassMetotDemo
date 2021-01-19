20d33ef4bfd9f449ade3600da3df4d0ecfa7ed60
sadimert <sadimertyl@gmail.com>
2021-01-19 09:25:29 +0300

    Add project files.

---
+++ ClassMetotDemo/MusteriMenager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class MusteriMenager
+    {
+       public void Ekle(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri Eklendi");
+            Console.WriteLine("Adi" + musteri.Ad);
+            Console.WriteLine("Soyadı" + musteri.Soyad);
+            Console.WriteLine("Telefon" + musteri.Telefon);
+            Console.WriteLine("E-Mail" + musteri.EMail);
+        }
+        public void Listele(Musteri musteri)
+        {
+            Console.WriteLine(musteri.Ad + "  " + musteri.Soyad);
+        }
+        public void Sil(Musteri musteri)
+        {
+            Console.WriteLine("Müşteri silindi");
+            Console.WriteLine(musteri.Ad + " " + musteri.Soyad);
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Musteri musteri1 = new Musteri();
+            musteri1.Ad = "Sadi Mert";
+            musteri1.Soyad = "YILMAZ";
+            musteri1.Telefon = 554;
+            musteri1.EMail = "sadimertyl@gmail.com";
+
+            Musteri musteri2 = new Musteri();
+            musteri2.Ad = "Ali";
+            musteri2.Soyad = "YILMAZ";
+            musteri2.Telefon = 542;
+            musteri2.EMail = "Aliii@gmail.com";
+
+            Musteri musteri3 = new Musteri();
+            musteri3.Ad = "Veli";
+            musteri3.Soyad = "YILMAZ";
+            musteri3.Telefon = 543;
+            musteri3.EMail = "Veliii@gmail.com";
+            Musteri[] musteriler = new Musteri[] { musteri1, musteri2, musteri3 };
+
+            MusteriMenager musteriManager = new MusteriMenager();
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.Ekle(musteri);
+                Console.WriteLine("---------------------------");
+
+            }
+            Console.WriteLine("Müşteriler:");
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.Listele(musteri);
+                Console.WriteLine("---------------------------");
+            }
+            Console.WriteLine("Müşteri Sil");
+            Console.WriteLine("---------------------------");
+
+            foreach (var musteri in musteriler)
+            {
+                musteriManager.Sil(musteri);
+                Console.WriteLine("---------------------------");
+            }
+        }
+    }
+}
